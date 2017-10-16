12fc71d6c815f506cf2ae7d13c586d6313e51ca0
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-16 09:49:23 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,12 +2,13 @@ class Backer
   attr_accessor :backed_projectes, :name
 
   def initialize(name)
+    binding.pry
     self.name = name
     self.backed_projectes = []
   end
 
   def back_project(project)
-    binding.pry
+    #binding.pry
     self.backed_projectes << project
   end
 
